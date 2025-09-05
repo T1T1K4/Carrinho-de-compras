efd20ca7889205829694b58b407e3720f2c64640
Carlos Eduardo <151068498+T1T1K4@users.noreply.github.com>
2025-09-05 19:33:04 -0300

    Delete carrinho valdir/carrinho valdir directory

--- carrinho valdir/carrinho valdir/includes/exibir_produtos.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Exibir produtos
-foreach ($produtos as $dadosProduto) {
-    $produto = new Produto($dadosProduto['id'], $dadosProduto['nome'], $dadosProduto['preco'], $dadosProduto['estoque']);
-    echo $produto->renderizarProduto();
-}
